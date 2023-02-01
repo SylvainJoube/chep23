281288b5a57f067d5daf1cf0d000632fca64d4c5
Sylvain Joube <sylvain.joube@gmail.com>
2023-02-01 15:37:45 +0100

    new structure for pt3D (to avoid the std:: namespace)

--- data_structures.hpp
+++ data_structures.hpp
@@ -15,6 +15,8 @@ namespace kwk
     std::size_t d0, d1;
     std::vector<T> elems;
 
+    matrix() = default;
+
     explicit matrix(std::size_t d0_, std::size_t d1_) : d0(d0_), d1(d1_)
     {
       elems.resize(d0 * d1);
@@ -24,6 +26,7 @@ namespace kwk
     {
       elems.resize(d0 * d1);
       for (uint i = 0; i < d0*d1; ++i) elems.at(i) = array[i];
+      // init(d0_, d1_, array);
     }
 
     T &operator()(std::size_t x, std::size_t y)
@@ -123,7 +126,36 @@ namespace kwk
 
 
 template<typename T>
-using pt3D = std::array<T, 3>;
+struct pt3D
+{
+  std::array<T, 3> data;
+  T& operator[](std::size_t i) { return data[i]; }
+  T  operator[](std::size_t i) const { return data[i]; }
+  auto begin()       { return data.begin(); }
+  auto begin() const { return data.begin(); }
+  auto end()         { return data.end(); }
+  auto end() const   { return data.end(); }
+};
+
+template<typename T>
+constexpr auto operator*(pt3D<T> a, T b) noexcept
+{
+  for (auto &e : a) e *= b;
+  return a;
+}
+
+template<typename T>
+constexpr auto operator+(pt3D<T> a, pt3D<T> b) noexcept
+{
+  for(int i=0;i<3;i++) a[i] += b[i];
+  return a;
+}
+
+template<typename T>
+constexpr auto operator*(T b, pt3D<T> a) noexcept
+{
+  return a * b;
+}
 
 template<typename T>
 inline std::ostream& operator<<(std::ostream &s, const pt3D<T> &p)
